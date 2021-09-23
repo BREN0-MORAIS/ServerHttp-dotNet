007f4b4ff63c539398c8bb668b95ae68e5acf110
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-09-22 21:39:54 -0400

    init

---
+++ ServerHttp/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startServerBtn = new System.Windows.Forms.Button();
+            this.stopServerBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverPortText = new System.Windows.Forms.TextBox();
+            this.serverLogsText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startServerBtn
+            // 
+            this.startServerBtn.Location = new System.Drawing.Point(25, 27);
+            this.startServerBtn.Name = "startServerBtn";
+            this.startServerBtn.Size = new System.Drawing.Size(87, 51);
+            this.startServerBtn.TabIndex = 0;
+            this.startServerBtn.Text = "Start";
+            this.startServerBtn.UseMnemonic = false;
+            this.startServerBtn.UseVisualStyleBackColor = true;
+            this.startServerBtn.Click += new System.EventHandler(this.startServerBtn_Click);
+            // 
+            // stopServerBtn
+            // 
+            this.stopServerBtn.Location = new System.Drawing.Point(25, 96);
+            this.stopServerBtn.Name = "stopServerBtn";
+            this.stopServerBtn.Size = new System.Drawing.Size(87, 53);
+            this.stopServerBtn.TabIndex = 1;
+            this.stopServerBtn.Text = "Stop";
+            this.stopServerBtn.UseVisualStyleBackColor = true;
+            this.stopServerBtn.Click += new System.EventHandler(this.stopServerBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Logs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // serverPortText
+            // 
+            this.serverPortText.Location = new System.Drawing.Point(12, 200);
+            this.serverPortText.Name = "serverPortText";
+            this.serverPortText.Size = new System.Drawing.Size(100, 23);
+            this.serverPortText.TabIndex = 4;
+            // 
+            // serverLogsText
+            // 
+            this.serverLogsText.Location = new System.Drawing.Point(139, 27);
+            this.serverLogsText.Multiline = true;
+            this.serverLogsText.Name = "serverLogsText";
+            this.serverLogsText.ReadOnly = true;
+            this.serverLogsText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.serverLogsText.Size = new System.Drawing.Size(649, 411);
+            this.serverLogsText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.serverLogsText);
+            this.Controls.Add(this.serverPortText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopServerBtn);
+            this.Controls.Add(this.startServerBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startServerBtn;
+        private System.Windows.Forms.Button stopServerBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverPortText;
+        private System.Windows.Forms.TextBox serverLogsText;
+    }
+}
+
